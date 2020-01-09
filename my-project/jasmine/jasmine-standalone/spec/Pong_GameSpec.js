f0b109393235efe270d80850fe421e6ee7e6ac5c
Mrugank Upadhyay <32528837+Mrugank-Upadhyay@users.noreply.github.com>
2020-01-08 19:31:56 -0500

    Update Pong_GameSpec.js

--- my-project/jasmine/jasmine-standalone/spec/Pong_GameSpec.js
+++ my-project/jasmine/jasmine-standalone/spec/Pong_GameSpec.js
@@ -1,27 +1,37 @@
-/*
-Developer's Note:
-
-	Once SpecRunner is executed, the first 2 tests will pass, while the thrid will not (may not be in the same order in browser). 
-	This can be noted from the green dots (pass) and red cross (fail) under "Jasmine" on the top-left. 
-	However, in the browser, an error is shown due to jasmine not being able to get a value for canvasContext.getContext 
-	within the game file. This does not impede the testing.
-*/
-
 describe("Sigmoid Curve Multiplier", function() {
 	it("should return new y speed", function() {
-		expect(sigmoidCurveMultiplier(30)).toEqual(20.841365175303885);
+		// Arrange
+		const num = 30;
+		
+		// Act
+		const result = sigmoidCurveMultiplier(num);
+		
+		// Assert
+		expect(result).toEqual(20.841365175303885);
 	});
 });
 
 describe("Staircase Curve Multiplier", function() {
 	it("should return new x speed", function() {
-		expect(staircaseCurveMultiplier(50)).toEqual(18.126452624390886);
+		//Arrange
+		const num = 50;
+		
+		// Act
+		const result = staircaseCurveMultiplier(num);
+		
+		// Assert
+		expect(result).toEqual(18.126452624390886);
 	});
 });
 
-//This test is intentionally made to fail
+
 describe("Staircase Curve Multiplier", function() {
 	it("should return new x speed", function() {
-		expect(staircaseCurveMultiplier(50)).toEqual(10);
+		// Arrange
+		const num = 0;
+		
+		// Act
+		const result = staircaseCurveMultiplier(num);
+		expect(result).toEqual(0);
 	});
 });
